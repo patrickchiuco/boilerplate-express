bf259b2ba9cd6282ef39d4e56e8ab1cb084c3cff
Patrick Michael Chiuco <patrick.chiuco@gmail.com>
2023-07-26 18:46:15 +0800

    Update myApp.js
    Challenge 2 continuation

--- myApp.js
+++ myApp.js
@@ -1,5 +1,12 @@
 let express = require('express');
 let app = express();
+console.log("Hello World!")
+//console.log(__dirname + "/views/index.html")
+app.get("/", function(req, res) {
+  //res.send("Hello Express")
+
+  res.sendFile(__dirname + "/views/index.html")
+});
 
 
 
@@ -35,5 +42,4 @@ let app = express();
 
 
 
-
- module.exports = app;
+module.exports = app;
