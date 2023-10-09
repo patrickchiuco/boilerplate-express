c26825a44aa0daba57b66f44d8bebaa1a0c88763
Patrick Michael Chiuco <patrick.chiuco@gmail.com>
2023-10-09 12:29:58 +0800

    Update myApp.js
    Challenge 7 done.

--- myApp.js
+++ myApp.js
@@ -2,6 +2,12 @@ let express = require('express');
 let app = express();
 console.log("Hello World!")
 //console.log(__dirname + "/views/index.html")
+app.use(function(req, res, next) {
+  let message = req.method + " " + req.path + " - " + req.ip;
+  console.log(message);
+  next();
+});
+app.use("/public", express.static(__dirname + "/public"))
 app.get("/", function(req, res) {
   //res.send("Hello Express")
 
@@ -9,37 +15,15 @@ app.get("/", function(req, res) {
 });
 
 
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
+app.get("/json", function(req, res) {
+  let message = "Hello json";
+  console.log(process.env["MESSAGE_STYLE"])
+  if (process.env["MESSAGE_STYLE"] === 'uppercase') {
+    res.json({ "message": message.toUpperCase() });
+  }
+  else {
+    res.json({ "message": message });
+  }
+});
 
 module.exports = app;
